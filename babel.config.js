b3a7ab7671e3bb14bb6db1a757eb09dc051a824e
HergaS <56360542+HergaS@users.noreply.github.com>
2020-02-10 00:33:13 +0100

    configuring travis CI

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,5 @@
-const path = require('path')
-const PrerenderSPAPlugin = require('prerender-spa-plugin')
-
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ],
-  plugins: [
-    ...
-    new PrerenderSPAPlugin({
-      // Required - The path to the webpack-outputted app to prerender.
-      staticDir: path.join(__dirname, 'dist'),
-      // Required - Routes to render.
-      routes: ['/'],
-    })
   ]
 }
